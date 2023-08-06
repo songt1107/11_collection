2984232a34d4ff3a76a2b772cdc33101bd4e1451
songt1107 <songt1107@gmail.com>
2023-08-06 21:18:45 +0900

    sunday

--- src/edu/kh/collection/run/SetRun.java
+++ src/edu/kh/collection/run/SetRun.java
@@ -7,9 +7,9 @@ public class SetRun {
 	public static void main(String[] args) {
 		
 		SetService service = new SetService();
-		//service.ex1();
-		//service.ex2();
-		//service.ex3();
+		service.ex1();
+		service.ex2();
+		service.ex3();
 		service.ex4();
 		service.lotto();
 	}
